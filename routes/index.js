6b37817fd5b25635d2f501ca7266f0e885cd737d
risman94 <kamarulrisman1994@gmail.com>
2017-09-06 08:53:38 +0800

    add post image to database

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,20 @@
 var express = require('express');
 var router = express.Router();
 var Image = require('./../models/image');
+var multer = require('multer');
+
+const storage = multer.diskStorage({
+	destination: (req, file, cb) => {
+		cb(null, "uploads/");
+	},
+	filename: (req, file, cb) => {
+		cb(null, file.originalname);
+	}
+});
+
+var upload = multer({storage: storage}).single('myimage');
+
+// var upload = multer({dest: 'uploads/'});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,12 +22,20 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', (req, res) => {
-	var data = new Image({
-		text: req.body.text,
-		image: req.body.image
-	});
-	data.save().then((result) => {
-		res.send(result);
+	upload(req, res, (err) => {
+		if (err) {
+			return res.end('error request file');
+		}
+		var data = new Image({
+			text: "text",
+			image: req.file.originalname
+		});
+		data.save().then((result) => {
+			res.send(result);
+		});
+		console.log(req.file);
+		res.end('upload file success');
+		console.log('success');
 	});
 });
 
